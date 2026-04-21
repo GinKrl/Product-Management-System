59dfe48a6394c5434707bcef77c2305d601171ec
Gian Karl Minglana <ginkrlming@gmail.com>
2026-04-21 22:51:58 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { AuthProvider } from './contexts/AuthContext'
-import { UserRightsProvider } from './contexts/UserRightsContext'
-import App from './App.jsx'
 import './index.css'
+import App from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext'
+import { UserRightsProvider } from './contexts/UserRightsContext' // Add this
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <AuthProvider>
-      <UserRightsProvider>
+      <UserRightsProvider> {/* Wrap App here */}
         <App />
       </UserRightsProvider>
     </AuthProvider>
-  </StrictMode>
+  </StrictMode>,
 )
